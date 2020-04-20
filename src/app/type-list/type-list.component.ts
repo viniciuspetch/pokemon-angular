d05f97a666fd10db44932b25a7ac989a2f8d6c84
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-04-20 19:25:40 -0300

    Initial type listing

--- src/app/type-list/type-list.component.ts
+++ src/app/type-list/type-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { typelist } from '../typelist'
 
 @Component({
   selector: 'app-type-list',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./type-list.component.css']
 })
 export class TypeListComponent implements OnInit {
-
+  typelist;
   constructor() { }
 
   ngOnInit(): void {
+    this.typelist = typelist;
   }
 
 }
