29a859d30a625fe0d3887f4d109024a87fce1c83
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-04-20 18:32:58 -0300

    Created some modules to start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { TypeListComponent } from './type-list/type-list.component';
+import { PokemonInfoComponent } from './pokemon-info/pokemon-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    TypeListComponent,
+    PokemonInfoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pokemon-info/pokemon-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-info',
+  templateUrl: './pokemon-info.component.html',
+  styleUrls: ['./pokemon-info.component.css']
+})
+export class PokemonInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
