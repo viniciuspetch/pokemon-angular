a7c7f0005307608713c57a2393a44d71dffa092a
Vinícius Araújo Petch <vinicius.ap21@gmail.com>
2020-04-21 15:30:35 -0300

    Added signout button on navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from './login.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private loginService: LoginService) { }
   title = 'pokemon-angular';
+
+  signout() {
+    this.loginService.clearToken()
+  }
 }
